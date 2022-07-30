e028197afda96b276d2c61209e8f2a2b4f1f9f7d
Thomas Basham <Bashamtg@gmail.com>
2022-07-30 11:05:20 -0700

    styled carousel, added player

--- pages/home.js
+++ pages/home.js
@@ -8,7 +8,7 @@ export default function Home() {
 
 const flickityOptions = {
 
-  freeScroll: true,
+  freeScroll: false,
   wrapAround: true
 }
 
@@ -25,12 +25,21 @@ const flickityOptions = {
       static // default false
 
     >
+        <div className="carousel-cell">
       <img alt='img' src="https://excision.ca/wp-content/uploads/2022/01/Excision_onyx_WebSlider_OUTNOW-1200x628.jpg" width={1000}/>
+      </div>
+      <div className="carousel-cell">
+
       <img alt='img' src="https://excision.ca/wp-content/uploads/2022/01/Excision_onyx_WebSlider_OUTNOW-1200x628.jpg" width={1000}/>
+      </div>
+      <div className="carousel-cell">
+
       <img alt='img' src="https://excision.ca/wp-content/uploads/2022/01/Excision_onyx_WebSlider_OUTNOW-1200x628.jpg" width={1000}/>
+      </div>
     </Flickity>
 
 
+    <iframe className='my-24' style={{borderRadius:"12px"}} src="https://open.spotify.com/embed/artist/7fVW0REbd7DTaQCKgXBbHm?utm_source=generator&theme=0" width="100%" height="380" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
     
       
       </>
