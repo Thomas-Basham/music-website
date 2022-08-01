f0d216153876c04fec5885698c6606ede310d74e
Thomas Basham <Bashamtg@gmail.com>
2022-08-01 15:22:40 -0700

    updated news article

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,35 +1,93 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '/styles/Home.module.css'
-import Header from '../../components/Header'
-import { Row } from 'react-bootstrap'
-import SpotifyPlayer from '../../components/SpotifyPlayer'
-import SpotifyCoverArt from '../../components/SpotifyCoverArt.js'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "/styles/Home.module.css";
+import Header from "../../components/Header";
+import { Row } from "react-bootstrap";
+import SpotifyPlayer from "../../components/SpotifyPlayer";
+import SpotifyCoverArt from "../../components/SpotifyCoverArt.js";
+
+import { SiPandora } from "react-icons/si";
+import {
+  FaApple,
+  FaAmazon,
+  FaDeezer,
+  FaSpotify,
+  FaItunes,
+} from "react-icons/fa";
+
 export default function LonelyMeCoverArt() {
-  
   // Go to spotify track and copy song link
-  const LonelyMeUrl = "http://open.spotify.com/track/0UJTi2a46HRnFD8wGFYjil?si=db7b62083e8c4253"
-  
+  const LonelyMeUrl =
+    "http://open.spotify.com/track/0UJTi2a46HRnFD8wGFYjil?si=db7b62083e8c4253";
+
   return (
-    
-      <>
+    <>
+      <Row lg={4} sm={1} className="pt-6 ">
+        <a
+          href="/news/lonely-me"
+          className="p-6 m-2 text-left text-white border-2 border-white w-100 text-decoration-none"
+        >
+          <h3>Lonely Me Out Now</h3>
+          <p className="float-left">January 7, 2022</p>
 
-      <Row lg={4} sm={1} className="pt-6 " >
-        
-        <a href='/news/lonely-me'  className='p-6 m-2 text-left text-white border-2 border-white w-100 text-decoration-none'>
-        <h3>Lonely Me Out Now</h3>
-        <p>January 7, 2022</p>
+          <div className="justify-around float-right ml-auto text-right w-25 d-flex text-decoration-none">
+            <a
+              href="https://found.ee/FEkeI"
+              className="text-white text-decoration-none"
+              title="Get it at Spotify"
+            >
+              <FaSpotify />
+            </a>
+            <a
+              href="https://found.ee/wlhci"
+              className="text-white text-decoration-none"
+              title="Get it at Apple Music"
+            >
+              <FaApple />
+            </a>
+            <a
+              href="https://found.ee/rI5Zt"
+              className="text-white text-decoration-none"
+              title="Get it at iTunes"
+            >
+              <FaItunes />
+            </a>
+            <a
+              href="https://found.ee/Qst2p"
+              className="text-white text-decoration-none"
+              title="Get it at Amazon"
+            >
+              <FaAmazon />
+            </a>
+            <a
+              href="https://found.ee/bI8ZBZ"
+              className="text-white text-decoration-none"
+              title="Get it at Pandora"
+            >
+              <SiPandora />
+            </a>
+            <a
+              href="https://found.ee/tcvzL1"
+              className="text-white text-decoration-none"
+              title="Get it at Deezer"
+            >
+              <FaDeezer />
+            </a>
+          </div>
 
-        
-        <div className='d-flex'>
-        <SpotifyCoverArt url={LonelyMeUrl}/>
-        {SpotifyPlayer("https://open.spotify.com/embed/track/0UJTi2a46HRnFD8wGFYjil?utm_source=generator&theme=0")}
-        </div>
+          <div className="justify-between clear-both d-flex" >
+            <SpotifyCoverArt url={LonelyMeUrl} />
+           <div className="" style={{width: "70%"}}>
+            {SpotifyPlayer(
+              "https://open.spotify.com/embed/track/0UJTi2a46HRnFD8wGFYjil?utm_source=generator&theme=0"
+            )}
+         </div>
+         
+          </div>
         </a>
-
       </Row>
-      </>
-  )
+    </>
+  );
 }
 
 // https://open.spotify.com/track/0UJTi2a46HRnFD8wGFYjil?si=db7b62083e8c4253
--- pages/news/lonely-me.js
+++ pages/news/lonely-me.js
@@ -11,10 +11,42 @@ export default function lonelyMeNewsPost() {
     "http://open.spotify.com/track/0UJTi2a46HRnFD8wGFYjil?si=db7b62083e8c4253";
 
   return (
-    <>
-      <p>Lonely Me By Thomas Basham</p>
-    </>
+    <div className="flex-col p-6 text-white opacity-90 d-flex">
+      <div className="justify-between d-flex">
+        <h3 className="clear-both">Lonely Me Out Now</h3>
+        <p className="ml-auto float-end ">January 7, 2022</p>
+      </div>
+
+      {SpotifyPlayer(
+        "https://open.spotify.com/embed/track/0UJTi2a46HRnFD8wGFYjil?utm_source=generator&theme=0"
+      )}
+      <p className="p-12 mt-6 border-2 border-white">
+        I think everyone goes through the feeling of loneliness at least once in
+        their life and everyone feels it and expresses it different. The lyrics
+        and vocals were written and performed by Stephano Prunebelli . I was
+        captivated by his ability to express his emotions, and his story of
+        loneliness. I hope you enjoy it! You can stream it on all major music
+        platforms
+        <br></br>
+        <br></br>
+        Some Days,I think of you Others, I'll let it slip through And I only
+        wish I knew how to say, say me too
+        <br></br>
+        <br></br>
+        Cause I don't how to be The other half of somebody What I do best is
+        leave that's why I'll always be a "Lonely Me" a "Lonely Me" a "Lonely
+        Me" a "Lonely Me"
+        <br></br>
+        <br></br>
+        Sometimes, I dream of you others, I stay up talking about you And I only
+        wish I knew how to say, say me too
+        <br></br>
+        <br></br>I wish My heart was broken, So I could give this up
+        <br></br>
+        <br></br>
+        Give up on love..
+      </p>
+    </div>
   );
 }
 
-// https://open.spotify.com/track/0UJTi2a46HRnFD8wGFYjil?si=db7b62083e8c4253
