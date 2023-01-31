ba16c54b73d3ea9d6f9c486580ead216cd2b10f5
Thomas Basham <Bashamtg@gmail.com>
2023-01-31 11:45:55 -0800

    updated css ;

--- components/SpotifyPlayer.js
+++ components/SpotifyPlayer.js
@@ -4,7 +4,7 @@ export default function SpotifyPlayer(props) {
   if (props.size == "small") { height = 80} else { height = 500}
 
   return (
-    <iframe className='my-px' style={{borderRadius:"12px"}} src={props.url} width="100%" height={height} allowTransparency='true' frameBorder="0"  allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <iframe className='my-px' style={{borderRadius:"12px"}} src={props.url} width="100%" height={height} allowtransparency='true' frameBorder="0"  allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 
   );
 }
--- components/layout.js
+++ components/layout.js
@@ -15,7 +15,7 @@ export default function Layout({ children }) {
           name="description"
           content="Tommy Basham's Official Website. Discover music, news, videos, and much more."
         />
-        <meta charset="UTF-8"/>
+        <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </Head>
 
